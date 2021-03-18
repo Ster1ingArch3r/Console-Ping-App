667410f7d0cb77e3eebbf20cdb5bb44038bf3979
Ster1ingArch3r <80054842+Ster1ingArch3r@users.noreply.github.com>
2021-03-18 10:43:51 -0400

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        public static void BuildList(string a, string b, string c, string d, string e)
+        {
+            Console.WriteLine("Your Selected IP Range is " + a + " through " + b + ". Is this correct?");
+            string answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                //build list of all IPs in range 
+                int numIpRange1 = Convert.ToInt32(c);
+                int numIpRange2 = Convert.ToInt32(d);
+                List<string> ipList = new List<string>();
+                for (int i = 0; i <= (numIpRange2 - numIpRange1); i++)
+                {
+                    ipList.Add(e + "." + (i + numIpRange1).ToString());
+
+                }
+                Console.WriteLine(string.Join(", \n", ipList));
+                Console.ReadLine();
+
+                //send IP list to pingip method
+                PingIp(ipList);
+            }
+        }
+        //method for pinging IP
+        public static void PingIp(List<String> a)
+        {
+            //Ping ip addresses in the list.
+            try
+            {
+                Ping p = new Ping();
+                for(int i = 0; i < a.Count; i++)
+                {
+                    PingReply pr = p.Send(a[i], 5000);
+                    if (pr != null)
+                    {
+                        Console.WriteLine("Status: " + pr.Status + " Time: " + pr.RoundtripTime.ToString() + " Address: " + pr.Address);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Danger Will Robinson, Danger! Time out has occurred.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //get IP info from user.
+                Console.WriteLine("Please enter the IP address Range: ");
+                Console.WriteLine("IP Address Base (0.0.0)");
+                string baseIp = Console.ReadLine();
+                Console.WriteLine("Enter the final octet of the IP Addresses.");
+                Console.WriteLine("IP Address 1: ");
+                string ipRange1 = Console.ReadLine();
+                Console.WriteLine("IP Address 2:");
+                string ipRange2 = Console.ReadLine();
+
+
+                string fullIp1 = baseIp + "." + ipRange1;
+                string fullIp2 = baseIp + "." + ipRange2;
+
+                //Checks the inputed values before moving on. 
+                static bool validip(string a, string b)
+                {
+                    string pattern = @"([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$";
+                    Regex check = new Regex(pattern);
+                    if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(b))
+                    {
+                        
+                        return false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("IP Addresses are valid.");
+                        return true;
+                    }
+                }
+                if (validip(fullIp1, fullIp2) == true)
+                {
+                    Class1.BuildList(fullIp1, fullIp2, ipRange1, ipRange2, baseIp);
+                }
+                else
+                {
+                    break;
+                }
+
+                Console.WriteLine("Would you like to continue? (Y/N)");
+
+                string ans = Console.ReadLine();
+
+                if (ans == "n" || ans == "N")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+
+                /*Console.WriteLine("Your Selected IP Range is " + fullIp1 + " through " + fullIp2 + ". Is this correct?");
+                string answer = Console.ReadLine();
+
+                //build list of all IPs in range 
+                int numIpRange1 = Convert.ToInt32(ipRange1);
+                int numIpRange2 = Convert.ToInt32(ipRange2);
+                List<string> ipList = new List<string>();
+                for (int i = 0; i <= (numIpRange2 - numIpRange1); i++)
+                {
+                    ipList.Add(baseIp + "." + (i + numIpRange1).ToString());
+
+                }
+                Console.WriteLine(string.Join(", \n", ipList));
+                Console.ReadLine();
+
+
+                */
+                /*if (answer == "yes")
+                {
+                    Ping p = new Ping();
+
+                }*/
+
+            }
+        }
+    }
+}
